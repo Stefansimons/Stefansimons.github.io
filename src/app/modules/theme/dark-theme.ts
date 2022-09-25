c2b05eace08f75b0c74036e266ae57533d5aa5fb
Stefansimons <simonovic520@gmail.com>
2022-09-25 17:18:56 +0200

    Minor layout changes, color variable.

--- src/app/modules/theme/dark-theme.ts
+++ src/app/modules/theme/dark-theme.ts
@@ -8,6 +8,7 @@ export const darkTheme: Theme = {
     '--color-primary-light-1': 'rgb(36, 37, 38)',
     '--color-primary-main-1': '#ffb400',
     '--color-primary-light-2': '#2b2b2b',
+    '--color-primary-central-content': '#2f3e3d',
     '--color-primary-light-3': 'rgb(250, 250, 250)', // Text main color
     '--color-primary-dark-1': 'rgb(255,255,255)',
     '--color-primary-dark-2': '#645e9e',
